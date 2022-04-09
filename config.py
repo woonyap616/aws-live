45cb72da7acb782aa0d0218a65d9fac665c467f9
woonyap616 <62168445+woonyap616@users.noreply.github.com>
2022-04-09 22:00:31 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cfxtpjtlhvow.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chongwoonyap-bucket"
+customregion = "us-east-1"
